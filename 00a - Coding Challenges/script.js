3fe2a00f30425ec287b4356a3c5288cc55a534fb
Felix Goh <felix.gyh@gmail.com>
2022-12-17 20:56:42 +0800

    Completed P2 Coding Challenge 2

--- 00a - Coding Challenges/script.js
+++ 00a - Coding Challenges/script.js
@@ -1,4 +1,15 @@
-const bill = 430;
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+"use strict";
 
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+function calcTip(bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+console.log(calcTip(100));
+
+const bills = [125, 555, 55];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+console.log(tips)
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+console.log(total)
