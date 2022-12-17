3e78edcdd7fbb2554db37e887b50bd4d9477eb2a
Felix Goh <felix.gyh@gmail.com>
2022-12-17 20:35:57 +0800

    Revert "Revert "Completed P1 Code Challenge #4""
    This reverts commit 8cfc5cf803ea8c7caea6c85264d005039d228292.

--- 00a - Coding Challenges/script.js
+++ 00a - Coding Challenges/script.js
@@ -1,22 +1,4 @@
-"use strict";
+const bill = 430;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
-const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
-
-// Test Data 1
-// const averageDolphins = calcAverage(44, 23, 71);
-// const averageKoalas = calcAverage(65, 54, 49);
-
-// Test Data 2
-const averageDolphins = calcAverage(85, 54, 41);
-const averageKoalas = calcAverage(23, 34, 27);
-
-console.log(averageDolphins, averageKoalas);
-
-function checkWinner(avg1, avg2) {
-    if (avg1 >= avg2 * 2) console.log(`Dolphins wins (${avg1} vs. ${avg2})`);
-    else if (avg2 >= avg1 * 2) console.log(`Koalas wins (${avg2} vs. ${avg1})`);
-    else console.log("No winner!");
-}
-
-checkWinner(averageDolphins, averageKoalas);
-checkWinner(100, 200);
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
