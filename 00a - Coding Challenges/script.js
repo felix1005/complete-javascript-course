8cfc5cf803ea8c7caea6c85264d005039d228292
Felix Goh <felix.gyh@gmail.com>
2022-12-17 17:17:05 +0800

    Revert "Completed P1 Code Challenge #4"
    This reverts commit 86f1db551a88eb365414d626da5aaa83f20cc545.

--- 00a - Coding Challenges/script.js
+++ 00a - Coding Challenges/script.js
@@ -1,4 +1,15 @@
-const bill = 430;
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+const averageDolphins = (97 + 112 + 90) / 3;
+const averageKoalas = (109 + 95 + 90) / 3;
+const minScore = 100;
 
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+console.log(averageDolphins, averageKoalas);
+
+if (averageDolphins > averageKoalas && averageDolphins >= minScore) {
+    console.log("The winner is Dolphins!");
+} else if (averageKoalas > averageDolphins && averageKoalas >= minScore) {
+    console.log("The winner is Koalas!");
+} else if (averageKoalas === averageDolphins && averageKoalas >= minScore) {
+    console.log("It is a DRAW!");
+} else {
+    console.log("No one wins the trophy!");
+}
