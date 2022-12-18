626b2f11d08a309b578f5b487c3e59973bcaf2f2
Felix Goh <felix.gyh@gmail.com>
2022-12-18 16:55:20 +0800

    Complete Developer Skills & Editor Setup Codding Challenge

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,16 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+function printForecast(arr) {
+  let text = "...";
+  for (let i = 0; i < arr.length; i++) {
+    text += `${arr[i]}ºC in ${i + 1} days...`;
+  }
+  console.log(text);
+}
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+printForecast(data1);
+printForecast(data2);
