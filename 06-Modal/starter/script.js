10bd4a53024f478e287796b0ccde2e6abe5ce531
Felix Goh <felix.gyh@gmail.com>
2022-12-18 23:27:18 +0800

    Halfway Modal Window Project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const modalBtn = document.querySelectorAll('.show-modal');
+
+const modal = document.querySelector('.modal').classList.remove('hidden');
+
+console.log(modal);
+console.log(modalBtn);
