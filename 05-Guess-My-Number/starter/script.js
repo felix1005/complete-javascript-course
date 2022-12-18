5ca02498892e883e60617010b5f750a02064eee9
Felix Goh <felix.gyh@gmail.com>
2022-12-18 22:36:20 +0800

    Completed Guess My Number Game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,47 @@
 'use strict';
+
+let score = 20;
+let highScore = 0;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+// document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (score > 0) {
+    if (guess === 0) {
+      document.querySelector('.message').textContent = '🥱 No number!!!';
+    } else if (guess > secretNumber) {
+      document.querySelector('.message').textContent = '🙃 Too High!!!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else if (guess < secretNumber) {
+      document.querySelector('.message').textContent = '😏 Too Low!!!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else if (guess === secretNumber) {
+      document.querySelector('.message').textContent = '🤩 Bingo!!!';
+      document.querySelector('body').style.backgroundColor = '#00bcd4';
+      document.querySelector('.number').style.width = '30rem';
+      document.querySelector('.number').textContent = secretNumber;
+      if (score > highScore) {
+        highScore = score;
+        document.querySelector('.highscore').textContent = score;
+      }
+    }
+  } else if (score === 0) {
+    document.querySelector('.message').textContent = '👻 Game Over!!!';
+  }
+
+  console.log(guess, typeof guess);
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
