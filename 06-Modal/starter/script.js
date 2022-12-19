a3a85916c675808adfab514b216c3a1c3ae7b925
Felix Goh <felix.gyh@gmail.com>
2022-12-19 20:23:41 +0800

    Completed Modal Window Project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -2,7 +2,31 @@
 
 const modalBtn = document.querySelectorAll('.show-modal');
 
-const modal = document.querySelector('.modal').classList.remove('hidden');
+const modal = document.querySelector('.modal');
 
-console.log(modal);
-console.log(modalBtn);
+const overlay = document.querySelector('.overlay');
+const modalClose = document.querySelector('.close-modal');
+
+const displayModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const hideModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+
+for (let i = 0; i < modalBtn.length; i++) {
+    modalBtn[i].addEventListener('click', displayModal);
+}
+
+modalClose.addEventListener('click', hideModal);
+
+overlay.addEventListener('click', hideModal);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) hideModal();
+});
+
