185b51a8e03fcb82e4fb450414ddea4b9d338535
Felix Goh <felix.gyh@gmail.com>
2022-12-30 16:31:48 +0800

    Completed A Closer Look at Function Coding Challenge 2

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.body.addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
